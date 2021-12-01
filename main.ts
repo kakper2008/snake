221f7e79e67f8fe0e7a2b44b548102f0e99668b0
kakper2008 <94959259+kakper2008@users.noreply.github.com>
2021-12-01 09:49:55 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    if (richting == 0) {
+        richting = 3
+    } else {
+        richting += -1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    while (levend == 0) {
+        if (richting == 0) {
+        	
+        }
+        if (richting == 1) {
+        	
+        }
+        if (richting == 2) {
+        	
+        }
+        if (richting == 3) {
+        	
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (richting == 3) {
+        richting = 0
+    } else {
+        richting += 1
+    }
+})
+let richting = 0
+let levend = 0
+levend = 0
+let lengte = 3
+let appel_x = 0
+let appel_y = 0
+richting = 0
+basic.forever(function () {
+	
+})
